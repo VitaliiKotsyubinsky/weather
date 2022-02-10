800938f6ff493e33584393177206dfb22bf47d9f
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2022-02-10 18:11:08 +0200

    add js-code for current day

--- scriptt.js
+++ scriptt.js
@@ -1,41 +1,55 @@
-// api.openweathermap.org / data / 2.5 / weather ? lat = { lat } & lon={ lon }& appid={API key }
-// id=2172797
 
-// fbd1c187f058db0d48e533deb9ed41aa
-let tempBlock = document.querySelector('.main-temp')
-let cityBlock = document.querySelector('.city')
-let imgBlock = document.querySelector('.weather-icon')
-let update_date = document.querySelector('#update-date')
-let local_date = document.querySelector('.main-date')
-let searchInp = document.querySelector('.weather-search-box')
+const searchInp = document.querySelector('.weather-search-box')
+searchInp.addEventListener('keypress', setQuery)
 
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchInp.value)
 
+    }
+}
 
 
 
+function getResults(query) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=fbd1c187f058db0d48e533deb9ed41aa`)
+        .then(weather => {
+            return weather.json()
+        }).then(displayResults)
+}
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f112804d60d2d72210568440f273d0ed')
+function displayResults(weather) {
 
-    .then(resp => { return resp.json() })
-    .then(data => {
+    let city = document.querySelector('.location .city')
+    city.innerText = `${weather.name}, ${weather.sys.country}`
 
+    console.log(weather)
+    let now = new Date()
+    let date = document.querySelector('.location .main-date')
+    date.innerText = dateBuilder(now)
 
+    let temp = document.querySelector('.location .main-temp')
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span >`
 
-    })
-    .catch(() => {
-    })
+    let hilow = document.querySelector('.location .main-hi-low ')
+    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max} °c`
 
+    let icon = document.querySelector('.location .weather-icon')
+    icon.HTMLImageElement = `${weather.weather.icon}`
 
+}
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-    // .then(function (resp) { return resp.json() })
-    // .then(function (data) {
-    //     console.log(data)
-    //     document.querySelector('.city').innerHTML = data.name
-    // })
-    // .catch(function () {
+    return `${day} ${date} ${month} ${year} `;
+}
 
-    // })
 
 
 
