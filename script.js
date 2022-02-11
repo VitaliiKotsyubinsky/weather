79d21e9211913b32bb2af008f770c151155813b3
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2022-02-11 12:24:23 +0200

    add adaptiv

--- script.js
+++ script.js
@@ -1,91 +1,63 @@
-const link = 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=fbd1c187f058db0d48e533deb9ed41aa'
 
-// const link = 'http://api.weatherstack.com/current?access_key=c913725c279d79f50a2ed3d0a1cdd595'
+const searchInp = document.querySelector('.weather-search-box')
+searchInp.addEventListener('keypress', setQuery)
 
-const first = document.getElementById('first')
-
-
-
-let store = {
-    name: 'Николаев',
-    temperature: 0,
-    observationTime: '00:00 AM',
-    isDay: 'yes',
-    description: '',
-    properties: {
-        cloudcover: {},
-        humidity: {},
-        windSpeed: {},
-        pressure: {},
-        uvIndex: {},
-        visibility: {},
-    },
-}
-
-
-
-const fetchData = async () => {
-    const result = await fetch(`${link}&query=${store.name}`)
-    const data = await result.json()
-
-    const {
-        current: {
-            cloudcover,
-            temperature,
-            humidity,
-            observation_time: observationTime,
-            pressure,
-            uv_index: uvIndex,
-            visibility,
-            is_day: isDay,
-            weather_descriptions: description,
-            wind_speed: windSpeed,
-        },
-    } = data
-
-
-
-    console.log(data)
-    store = {
-        ...store,
-        temperature,
-        localTimeEpoch,
-        isDay,
-        description: '',
-        properties: {
-            cloudcover: {},
-            humidity: {},
-            windSpeed: {},
-            pressure: {},
-            uvIndex: {},
-            visibility: {},
-        }
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchInp.value)
 
     }
-    renderComponent()
 }
 
 
 
-const markup = () => {
-    const { name, description, localTime, temperature, isDay, properties } = store
-    return ` 
-        
-        <section class="location">
-        <div class="city">${name}</div>
-        <div class="weather-icon"></div>
-        <div class="main-temp">${temperature}<spam>°c</spam>
-        </div>
-        <div class="main-weather">Sunny</div>
-        <div class="main-hi-low">18°c / 21°c</div>
-        <div class="main-date">${observationTime}</div>
-        </section>
-        
-    `
+function getResults(query) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=fbd1c187f058db0d48e533deb9ed41aa`)
+        .then(weather => {
+            return weather.json()
+        }).then(displayResults)
 }
 
-function renderComponent() {
-    first.innerHTML = markup()
+function displayResults(weather) {
+
+    let city = document.querySelector('.location .city')
+    city.innerText = `${weather.name}, ${weather.sys.country}`
+
+    console.log(weather)
+    let now = new Date()
+    let date = document.querySelector('.location .main-date')
+    date.innerText = dateBuilder(now)
+
+    let temp = document.querySelector('.location .main-temp')
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span >`
+
+    let hilow = document.querySelector('.location .main-hi-low ')
+    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max} °c`
+
+    let icon = document.querySelector('.location .weather-icon')
+    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.weather[0]['icon']}@2x.png">`
+
+    let mainWeather = document.querySelector('.location .main-weather')
+    mainWeather.innerText = `${weather.weather[0]['main']}`
 }
 
-fetchData()
+
+
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year} `;
+}
+
+
+
+
+
+
+
