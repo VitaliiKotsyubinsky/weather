d21e1fe61753f4cbdd8b0bd0d6ca239efc216eb7
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2022-02-13 17:44:38 +0200

    add days forecastData

--- script.js
+++ script.js
@@ -12,16 +12,18 @@ function setQuery(evt) {
 
 
 function getResults(query) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&cnt=3&units=metric&appid=fbd1c187f058db0d48e533deb9ed41aa`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&units=metric&appid=fbd1c187f058db0d48e533deb9ed41aa`)
         .then(weather => {
             return weather.json()
         }).then(displayResults)
+        .then(console.log)
+
 }
 
 function displayResults(weather) {
 
     let city = document.querySelector('.location .city')
-    city.innerText = `${weather.name}, ${weather.sys.country}`
+    city.innerText = `${weather.city.name}, ${weather.city.country}`
 
     console.log(weather)
     let now = new Date()
@@ -29,19 +31,58 @@ function displayResults(weather) {
     date.innerText = dateBuilder(now)
 
     let temp = document.querySelector('.location .main-temp')
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span >`
+    temp.innerHTML = `${Math.round(weather.list[0].main.temp)}<span>°c</span >`
 
     let hilow = document.querySelector('.location .main-hi-low ')
-    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max} °c`
+    hilow.innerText = `${weather.list[0].main.temp_min}°c / ${weather.list[0].main.temp_max} °c`
 
     let icon = document.querySelector('.location .weather-icon')
-    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.weather[0]['icon']}@2x.png">`
+    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.list[0].weather[0]['icon']}@2x.png">`
 
     let mainWeather = document.querySelector('.location .main-weather')
-    mainWeather.innerText = `${weather.weather[0]['main']}`
+    mainWeather.innerText = `${weather.list[0].weather[0]['main']}`
+
+    console.log(weather)
+
+
+
+    let datesDiv = document.querySelectorAll('.dates')
+    for (let i = 0; i < datesDiv.length; i++) {
+        datesDiv[i].innerHTML = `${weather.list[i * 8].dt_txt}`
+    }
+
+    let weatherIconsDiv = document.querySelectorAll('.days .weather-icon')
+    for (let i = 0; i < weatherIconsDiv.length; i++) {
+        weatherIconsDiv[i].innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.list[i * 8].weather[0]['icon']}@2x.png">`
+    }
+
+    const obj = weather.list.reduce((obj, data) => {
+        const date = data.dt_txt.slice(0, 10)
+        obj[date] = [...obj[date] ?? [], data]
+        return obj
+    }, {})
+
+    const forecastData = Object.values(obj)
+    const myForecastData = []
+    for (let i = 1; i < 5; i++) {
+        let { dt_txt, main: { temp, temp_max, temp_min }, weather: [{ icon, description }] } = forecastData[i][5]
+        temp = Math.round(temp)
+        myForecastData.push({ dt_txt, temp, icon, temp_max, temp_min, description })
+    }
+
+    const dayDivs = document.querySelectorAll('.day')
+    for (let i = 0; i < myForecastData.length; i++) {
+        const { dt_txt, temp, icon, description } = myForecastData[i]
+        dayDivs[i].innerHTML = `
+            <div class="dates">${dt_txt}</div>
+            <div class="weather-icon"><img src="http://openweathermap.org/img/wn/${icon}@2x.png"></div>
+            <div class="temps">${temp}<spam>°c</spam></div>            
+            <div class="weathers">${description}</div>          
+        `
+    }
+    return obj
+
 }
-
-
 
 function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -54,6 +95,11 @@ function dateBuilder(d) {
 
     return `${day} ${date} ${month} ${year} `;
 }
+
+
+
+
+
 
 
 
